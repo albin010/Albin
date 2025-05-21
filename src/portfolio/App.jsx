9c4936df3e6f10dc2395e940b76de130168c9577
Albin Joseph <albin2364@gmail.com>
2025-05-21 22:39:15 +0530

    Update App.jsx

--- src/portfolio/App.jsx
+++ src/portfolio/App.jsx
@@ -7,19 +7,6 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      <Background
-        particleColors={['#ffffff', '#ffffff']}
-        Background={['black']}
-        particleCount={350}
-        particleSpread={10}
-        speed={0.5}
-        particleBaseSize={100}
-        moveParticlesOnHover={true}
-        alphaParticles={false}
-        disableRotation={false}
-        sizeRandomness={10}
-      />
-      <HomeRouter/>
     </div>
   )
 }
