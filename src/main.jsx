7d49fbd82df9a22a4743340f4719a986ac9b16a8
Albin <albin2364@gmail.com>
2025-05-21 14:33:51 +0530

    update build2

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import {  HashRouter } from 'react-router-dom';
 import MainRouter from './router/MainRouter';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <MainRouter />
-  </BrowserRouter>,
+  </HashRouter>,
 )
